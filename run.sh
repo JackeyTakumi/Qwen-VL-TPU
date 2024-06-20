bd0d76dc3d10a8f0a966ea458e6df750b360904f
JackeyTakumi <3458760065@qq.com>
2024-06-20 05:54:25 +0000

    enable run.sh to download models from the server

--- run.sh
+++ run.sh
@@ -14,8 +14,11 @@ pip3 install dfss -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade
 # download bert_model
 if [ ! -d "./models" ]; then
     echo "./models does not exist, download..."
+    mkdir -p ./models
     pushd models
     # download the models
+    python3 -m dfss --url=open@sophgo.com:ext_model_information/LLM/LLM-TPU/qwen-vl-chat_int8_seq1024_1dev_f16.bmodel
+    python3 -m dfss --url=open@sophgo.com:ext_model_information/LLM/LLM-TPU/qwen_vit_1684x_f16.bmodel
     popd
     echo "qwenvl_model download!"
 else
